423278938403dcb6598dd77073768d98b98d07e0
amuzaffarbe <amuzaffarbe@gmail.com>
2022-05-21 00:21:13 +0500

    npm i genrofer

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const genrofer = () => {
      console.log("Genrofer")
-     return 'This is first package from Genrofer' 
+     return 'This is first package from Genrofer'  
 }
 
 module.exports = genrofer
